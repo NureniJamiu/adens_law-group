db853ad5c5a45eaa9c0391ed47e0b37aaca58a2c
NureniJamiu <nurenijamiu951@gmail.com>
2023-01-04 14:30:40 +0100

    feat: setup project file structure

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Header } from "./containers";
+import { Navbar } from "./components";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Header />
+    </div>
+  );
+}
+
+export default App;
